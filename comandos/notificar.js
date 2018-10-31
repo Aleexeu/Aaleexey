2bc063adc5913e9bce63ad0a665d9ee5ef5712e4
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-10-31 13:45:00 -0300

    Update notificar.js

--- comandos/notificar.js
+++ comandos/notificar.js
@@ -4,7 +4,7 @@ exports.run = (client, message, args) => {
     if(message.author.bot) return;
     if(message.channel.id === "502661109149466645") {
     if(message.content === "b!notificar") message.member.addRole("507231389608050689")
-       message.channel.send(':Feliz: agora voce contém o cargo 📣 | Notificado')
+       message.channel.send('<a:Yay:501026262534324224> agora voce contém o cargo 📣 | Notificado')
     }
     
 }
