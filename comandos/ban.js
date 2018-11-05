95732da2f72a76818ef82932eb870c967b8c330f
Aleexeu <42278872+Aleexeu@users.noreply.github.com>
2018-11-05 13:02:49 -0200

    Update ban.js

--- comandos/ban.js
+++ comandos/ban.js
@@ -2,10 +2,18 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let bUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!bUser) return message.channel.send("Não consigo encontrar usuário!");
+    if(!bUser) return message.channel.send({embed: {
+        description: "Você digitou b!ban, talvez precise de ajuda ?\n \nSe você for membro o comando vai ser bloqueado, agora se você é da staff, digite:\n \n b!ban @membro",
+        color: 0xbc0076,
+        footer: {
+            "icon_url": message.author.displayAvatarURL,
+            "text": "Vector | Todos os direitos reservados."
+          },
+        }
+        })
     let bReason = args.join(" ").slice(22);
     if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Infelizmente você não tem permissão!");
-    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Essa pessoa não pode ser chutada!");
+    if(bUser.hasPermission("BAN_MEMBERS")) return message.channel.send("Desculpa mais eu acho que ele tem um cargo acima do meu, ou eu não tenho permissão.");
 
     let banEmbed = new Discord.RichEmbed()
     .setDescription("BAN")
@@ -16,7 +24,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("Time", message.createdAt)
     .addField("Motivo:", bReason);
 
-    let incidentchannel = message.guild.channels.find(`name`, "puniçõess");
+    let incidentchannel = message.guild.channels.find(`name`, "punições");
     if(!incidentchannel) return message.channel.send("Não encontrei o canal #punições.");
 
     message.guild.member(bUser).ban(bReason);
